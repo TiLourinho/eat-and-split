a533b4d51a8103de6a876a8db85a0e722b049c52
Tiago Lourinho <lourinho.tiago@gmail.com>
2025-09-04 15:41:17 -0300

    Create static FormAddFriend component

---
+++ src/components/FormAddFriend.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label htmlFor="name">🧑🏻‍🤝‍👩🏼 Name</label>
+      <input type="text" id="name" />
+      <label htmlFor="image">🖼️ Image URL</label>
+      <input type="text" id="image" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+export default FormAddFriend;
